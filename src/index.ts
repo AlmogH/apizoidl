6c5bf61cf85d737c5b2b4bad0dd8571d28a8bc1d
AlmogH <almoghagbi96@gmail.com>
2023-01-23 16:39:54 +0200

    previous commit file weren"t added

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+/**
+ * Required External Modules
+ */
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+dotenv.config();
+
+/**
+ * App Variables
+ */
+
+
+const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
+
+const app = express();
+
+/**
+ *  App Configuration
+ */
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+
+/**
+ * Server Activation
+ */
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
