a82952087440628370b59b411e581ae653628aff
AlmogH <almoghagbi96@gmail.com>
2023-02-23 13:41:35 +0200

    Add comments to auth.controller.test

--- test/unit/controllers/auth.controller.test.ts
+++ test/unit/controllers/auth.controller.test.ts
@@ -11,12 +11,20 @@ import {
 	afterAll,
 } from "@jest/globals";
 
+// import mocks for OAuth2Client class from google-auth-library package for unit testing
 import { OAuth2ClientMock } from "../../testUtils/mocks";
 OAuth2ClientMock();
+
+// Import the express Request and Response interfaces
 import { NextFunction, Request, Response } from "express";
 import { OAuth2Client } from "google-auth-library";
+
+// Import the GoogleUtils class
 import { GoogleUtils } from "../../../src/utils/googleAuth.util";
+
+// Import the fs module
 import fs from "fs";
+
 /**
  * Unit tests for the AuthController class in src/controllers/auth.controller.ts
  */
@@ -24,17 +32,21 @@ import fs from "fs";
 // Import the AuthController class
 import { AuthController } from "../../../src/controllers/auth.controller";
 
+// describe the AuthController class for unit testing and define the unit tests
 describe("AuthController unit tests", () => {
 	let request: Request;
 	let response: Response;
 	let next: NextFunction;
 
-	// beforeAll(() => {});
-
+	// Restore all mocks after all tests are done
 	afterAll(() => {
 		jest.restoreAllMocks();
 	});
 
+	/**
+	 * Mock the fs.readFileSync method to return a mock client_id and client_secret
+	 * mock the express Request, Response and NextFunction interfaces
+	 */
 	beforeEach(() => {
 		jest.spyOn(fs, "readFileSync").mockImplementationOnce(() => {
 			return Buffer.from(
@@ -58,17 +70,24 @@ describe("AuthController unit tests", () => {
 		next = jest.fn() as NextFunction;
 	});
 
+	// test the AuthController class constructor
 	test("should create an instance of AuthController", () => {
 		const authController = new AuthController();
 		expect(authController).toBeInstanceOf(AuthController);
 	});
 
+	// test the oauth2Client property of the AuthController class
 	test("should have a property oauth2Client", () => {
 		const authController = new AuthController();
 		expect(authController).toHaveProperty("oauth2Client");
 	});
 
+	/**
+	 * Test the googleAuthHandler method of the AuthController class
+	 * should call response.sendStatus with 200 if client is not null
+	 */
 	test("googleAuthHandler should call response.sendStatus with 200 if client is not null", async () => {
+		// mock the loadSavedCredentialsIfExist method of the GoogleUtils class to return a mock OAuth2Client
 		jest
 			.spyOn(GoogleUtils, "loadSavedCredentialsIfExist")
 			.mockResolvedValue({} as OAuth2Client);
@@ -77,17 +96,24 @@ describe("AuthController unit tests", () => {
 		expect(response.sendStatus).toHaveBeenCalledWith(200);
 	});
 
+	/**
+	 * Test the googleAuthHandler method of the AuthController class
+	 * should call response.writeHead with 301 and location if client is null
+	 */
 	test("googleAuthHandler should call response.writeHead with 301 and location if client is null", async () => {
+		// mock the generateAuthUrl method of the OAuth2Client class to return a random string
 		OAuth2Client.prototype.generateAuthUrl = jest
 			.fn()
 			.mockReturnValue("random-string") as (opts?: undefined) => string;
 
+		// mock the loadSavedCredentialsIfExist method of the GoogleUtils class to return null
 		const loadSavedCredentialsIfExistMock = jest
 			.spyOn(GoogleUtils, "loadSavedCredentialsIfExist")
 			.mockResolvedValue(null);
 
 		const authController = new AuthController();
 		await authController.googleAuthHandler(request, response);
+		// expect the response.writeHead method to be called with 301 and location
 		expect(response.writeHead).toHaveBeenCalledWith(301, {
 			location: "random-string",
 		});
@@ -96,13 +122,16 @@ describe("AuthController unit tests", () => {
 	});
 
 	test("getTokenHandler should call response.sendStatus with 200 if client is not null", async () => {
+		// mock the loadSavedCredentialsIfExist method of the GoogleUtils class to return a mock OAuth2Client
 		GoogleUtils.saveCredentials = jest.fn<typeof GoogleUtils.saveCredentials>();
 
+		// mock the getToken method of the OAuth2Client class to return a mock tokens object
 		const mockedGetToken = jest.spyOn(OAuth2Client.prototype, "getToken");
 		mockedGetToken.mockImplementationOnce(() => {
 			return { tokens: "random-string" };
 		});
 
+		// mock the setCredentials method of the OAuth2Client class
 		const mockedSetCredentials = jest.spyOn(
 			OAuth2Client.prototype,
 			"setCredentials"
