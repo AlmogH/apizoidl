ea201f74bd5fd64b700abd8e05bd89c12b919378
AlmogH <almoghagbi96@gmail.com>
2023-03-06 15:20:26 +0200

    Update error middleware to log errors

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,5 +1,6 @@
 import { HttpException } from "../interfaces/httpException.interface";
 import { NextFunction, Request, Response } from "express";
+import logger from "../utils/logger.util";
 
 export const errorMiddleware = (
 	error: HttpException,
@@ -9,5 +10,6 @@ export const errorMiddleware = (
 ) => {
 	const status = error.status || 500;
 	const message = error.message || "Something went wrong";
+	logger.error(`${request.method} ${request.path} - ${status} - ${message}`);
 	response.status(status).send(message);
 };
