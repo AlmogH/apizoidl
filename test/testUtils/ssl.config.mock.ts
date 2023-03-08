dd156676b25feb666cfd2790e3a3f67f73d1bdd6
AlmogH <almoghagbi96@gmail.com>
2023-03-08 14:12:49 +0200

    fix ssl.config mock

--- test/testUtils/ssl.config.mock.ts
+++ test/testUtils/ssl.config.mock.ts
@@ -1,3 +1,12 @@
 // Workaround for jest.mock() not working with TypeScript
 import { jest } from "@jest/globals";
-jest.mock("../../src/configs/ssl.config");
+
+jest.mock("../../src/configs/ssl.config", () => {
+	return {
+		__esModule: true,
+		default: {
+			key: "key",
+			cert: "cert",
+		},
+	};
+});
